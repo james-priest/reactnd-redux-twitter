8b8036957d20acfb960335dc0ef20e57fde08b97
James Priest <james-priest@outlook.com>
2019-03-21 14:10:44 -0700

    feat: Add thunk & logger middleware

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -1,6 +1,6 @@
 import { RECEIVE_TWEETS } from '../actions/tweets';
 
-export default function users(state = {}, action) {
+export default function tweets(state = {}, action) {
   switch (action.type) {
     case RECEIVE_TWEETS:
       return {
