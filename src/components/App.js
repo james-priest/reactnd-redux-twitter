fcf70692dd5dc09d9296d49f6e9bc086a8447f8b
James Priest <james-priest@outlook.com>
2019-03-21 17:16:07 -0700

    feat: Add dispatch to get initial data

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import { handleInitialData } from '../actions/shared';
+import { connect } from 'react-redux';
 
 class App extends Component {
+  componentDidMount() {
+    const { dispatch } = this.props;
+
+    dispatch(handleInitialData());
+  }
   render() {
     return <div>Starter Code</div>;
   }
 }
 
-export default App;
+export default connect()(App);
