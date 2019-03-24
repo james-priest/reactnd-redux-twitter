29b41c9cc9e8675d158c470952cb5839724e2e37
James Priest <james-priest@outlook.com>
2019-03-23 22:51:48 -0700

    feat: Add Tweet UI

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,17 +1,66 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { formatTweet } from '../utils/helpers';
+import { formatTweet, formatDate } from '../utils/helpers';
+import {
+  TiArrowBackOutline,
+  TiHeartOutline,
+  TiHeartFullOutline
+} from 'react-icons/ti/index';
 
 export class Tweet extends Component {
+  handleLike = e => {
+    e.preventDefault();
+    // toggle like
+  };
+  toParent = (e, id) => {
+    e.preventDefault();
+    // redirect to the parent tweet
+  };
   render() {
     const { tweet } = this.props;
     if (tweet === null) {
       return <p>This tweet doesn't exist</p>;
     }
-    console.log(this.props);
+    const {
+      name,
+      avatar,
+      timestamp,
+      text,
+      hasLiked,
+      likes,
+      replies,
+      parent
+    } = tweet;
     return (
       <div className="tweet">
-        <h3>tweet</h3>
+        <img src={avatar} alt={`Avatar of ${name}`} className="avatar" />
+        <div className="tweet-info">
+          <div>
+            <span>{name}</span>
+            <div>{formatDate(timestamp)}</div>
+            {parent && (
+              <button
+                className="replying-to"
+                onClick={e => this.toParent(e, parent.id)}
+              >
+                Replying to @{parent.author}
+              </button>
+            )}
+            <p>{text}</p>
+          </div>
+          <div className="tweet-icons">
+            <TiArrowBackOutline className="tweet-icon" />
+            <span>{replies !== 0 && replies}</span>
+            <button className="heart-button" onClick={this.handleLike}>
+              {hasLiked === true ? (
+                <TiHeartFullOutline color="#e0245e" className="tweet-icon" />
+              ) : (
+                <TiHeartOutline className="tweet-icon" />
+              )}
+            </button>
+            <span>{likes !== 0 && likes}</span>
+          </div>
+        </div>
       </div>
     );
   }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux';
-import authedUsers from '../reducers/authedUser';
+import authedUser from '../reducers/authedUser';
 import users from '../reducers/users';
 import tweets from '../reducers/tweets';
 
 export default combineReducers({
-  authedUsers,
+  authedUser,
   users,
   tweets
 });
