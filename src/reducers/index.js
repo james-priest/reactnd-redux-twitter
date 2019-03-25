df83ba402b832af3018853dff4d35f6fad68b28c
James Priest <james-priest@outlook.com>
2019-03-24 21:44:16 -0700

    feat: Add Loading Bar

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import authedUser from '../reducers/authedUser';
 import users from '../reducers/users';
 import tweets from '../reducers/tweets';
+import { loadingBarReducer } from 'react-redux-loading';
 
 export default combineReducers({
   authedUser,
   users,
-  tweets
+  tweets,
+  loadingBar: loadingBarReducer
 });
