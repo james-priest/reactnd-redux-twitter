521d0ee33aa65aef369ece8a5244ec93e8123b29
James Priest <james-priest@outlook.com>
2019-03-27 11:12:45 -0700

    feat: Add New Tweet UI

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { handleInitialData } from '../actions/shared';
 import { connect } from 'react-redux';
 import Dashboard from './Dashboard';
 import LoadingBar from 'react-redux-loading';
+import NewTweet from './NewTweet';
 
 class App extends Component {
   componentDidMount() {
@@ -12,7 +13,7 @@ class App extends Component {
     return (
       <div>
         <LoadingBar />
-        {this.props.loading === true ? null : <Dashboard />}
+        {this.props.loading === true ? null : <NewTweet />}
       </div>
     );
   }
---
+++ src/components/NewTweet.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+
+export class NewTweet extends Component {
+  state = {
+    text: ''
+  };
+  handleChange = e => {
+    const text = e.target.value;
+
+    this.setState(() => ({
+      text
+    }));
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const { text } = this.state;
+
+    // todo: Add tweet to store
+
+    console.log('New Tweet:', text);
+
+    this.setState(() => ({
+      text: ''
+    }));
+  };
+  render() {
+    const { text } = this.state;
+
+    // todo: Redirect if submitted
+
+    const tweetLeft = 280 - text.length;
+
+    return (
+      <div>
+        <h3 className="center">Compose new Tweet</h3>
+        <form className="new-tweet" onSubmit={this.handleSubmit}>
+          <textarea
+            placeholder="What's happening?"
+            value={text}
+            onChange={this.handleChange}
+            className="textarea"
+            maxLength={280}
+          />
+          {tweetLeft <= 100 && <div className="tweet-length">{tweetLeft}</div>}
+          <button className="btn" type="submit" disabled={text === ''}>
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default NewTweet;
