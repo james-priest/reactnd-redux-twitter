38a2b675c37122338c552355bb2a6180f712aa2d
James Priest <james-priest@outlook.com>
2019-03-28 21:57:41 -0700

    feat: Add Redux DevTools addin code

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers/index';
 import middleware from './middleware';
 
-const store = createStore(reducer, middleware);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(middleware));
 
 ReactDOM.render(
   <Provider store={store}>
